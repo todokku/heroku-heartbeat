3e55a83879dd2c07d17fbe83f45b25a2bd43a532
Jared Jewitt <jaredjewitt@gmail.com>
2020-05-22 12:19:48 -0700

    Add Docker config (#2)
    Co-authored-by: Jared Jewitt <jared@freshworks.io>

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,33 @@
-import { serve } from 'https://deno.land/std@0.50.0/http/server.ts';
+import 'https://deno.land/x/dotenv/load.ts';
 
-const s = serve({ port: process.env.PORT || 5000 });
-
-console.log('http://localhost:5000/');
-
-for await (const req of s) {
-  req.respond({ body: 'Hello World\n' });
+interface IApp {
+  web_url: string;
 }
+
+const TWENTY_FIVE_MINUTES = 25 * 60000;
+
+/**
+ * Get the Heroku API key.
+ */
+const HEROKU_API_KEY = Deno.env.get('HEROKU_API_KEY');
+
+/**
+ * Get all the apps associated with the Heroku API key.
+ */
+const response = await fetch('https://api.heroku.com/apps', {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/vnd.heroku+json; version=3',
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${HEROKU_API_KEY}`,
+  },
+});
+
+/**
+ * Ping all the apps every 25 minutes.
+ */
+const apps = await response.json();
+setInterval(async () => {
+  await Promise.all(apps.map((app: IApp) => fetch(app.web_url)));
+}, TWENTY_FIVE_MINUTES);
+
